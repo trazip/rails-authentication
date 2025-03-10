283e7e55a4f2080aed498071820a48751fac4868
trazip <p.penhard@gmail.com>
2025-03-10 10:22:11 +0100

    Stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+  end
+
+  def private
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+  end
+
+  private
+  
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :addres)
   end
 end
